7757e3b8f285ed3bda242f341330ea5e1b30929c
Mahdi Ghaisi <mmghf74@gmail.com>
2022-04-30 23:11:47 +0430

    MNIST dataset loaded

--- C_code/main.c
+++ C_code/main.c
@@ -1,12 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    FILE *fp = fopen("../t10k-labels.idx1-ubyte", "rb");
-    unsigned char *buffer = (unsigned char *) malloc(4);
-    fseek(fp, 0, SEEK_SET);
+#define magic_number_size 4
+
+int number_of_train_images;
+int number_of_test_images;
+int rows;
+int columns;
+int *train_labels;
+int *test_labels;
+int **train_images;
+int **test_images;
+
+int readInteger(FILE *fp) {
+
+    unsigned char buffer[4];
     fread(buffer, 1, 4, fp);
-    for (int i = 0; i < 4; i++)
-        printf("%u\n", buffer[i]);
+
+    int result = 0;
+    for (int i = 0; i < 4; i++) {
+        result *= 256;
+        result += buffer[i];
+    }
+
+    return result;
+}
+
+int **readImages(char *address, int t_s) {
+
+    FILE *fp = fopen(address, "rb");
+    fseek(fp, magic_number_size, SEEK_SET);
+
+    int images = readInteger(fp);
+    rows = readInteger(fp);
+    columns = readInteger(fp);
+    int pixels = rows * columns;
+
+    int **data = (int **) malloc(images * sizeof(int *));
+    for (int i = 0; i < images; i++)
+        data[i] = (int *) malloc(pixels * sizeof(int));
+
+    unsigned char pixel;
+    for (int i = 0; i < images; i++) {
+
+        for (int j = 0; j < pixels; j++) {
+
+            fread(&pixel, 1, 1, fp);
+            data[i][j] = pixel;
+        }
+    }
+
+    // variable t_s indicates whether this function is reading train data or test data
+    if (t_s == 1)
+        number_of_train_images = images;
+    else
+        number_of_test_images = images;
+
+    return data;
+}
+
+int *readLabels(char *address) {
+
+    FILE *fp = fopen(address, "rb");
+    fseek(fp, magic_number_size, SEEK_SET);
+
+    int items = readInteger(fp);
+    int *labels = (int *) malloc(items * sizeof(int));
+    unsigned char label;
+    for (int i = 0; i < items; i++) {
+        fread(&label, 1, 1, fp);
+        labels[i] = label;
+    }
+
+    return labels;
+}
+
+void getDataset() {
+
+    train_images = readImages("D:\\computer\\ComputationalIntelligence\\NeuralNetwork\\p1\\train-images.idx3-ubyte", 1);
+    test_images = readImages("D:\\computer\\ComputationalIntelligence\\NeuralNetwork\\p1\\t10k-images.idx3-ubyte", 0);
+    train_labels = readLabels("D:\\computer\\ComputationalIntelligence\\NeuralNetwork\\p1\\train-labels.idx1-ubyte");
+    test_labels = readLabels("D:\\computer\\ComputationalIntelligence\\NeuralNetwork\\p1\\t10k-labels.idx1-ubyte");
+}
+
+int main() {
+    getDataset();
     return 0;
 }
